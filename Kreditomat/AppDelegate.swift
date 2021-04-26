d34fbc8b8dc53c3ca103707ee7ec532428bcf826
Chingiz <k.chyngiz@gmail.com>
2021-04-26 18:49:27 +0600

    addeed auth

--- Kreditomat/AppDelegate.swift
+++ Kreditomat/AppDelegate.swift
@@ -1,13 +1,115 @@
 import UIKit
+import CoreData
+import Swinject
+import IQKeyboardManagerSwift
+import Kingfisher
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    var window: UIWindow?
+    let assembler = Assembler([DependencyContainerAssembly()])
+    private var appCoordinator: AppCoordinator?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
+    
+    private func setupWindow() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = CoordinatorNavigationController(backBarButtonImage: nil)
+        window?.makeKeyAndVisible()
+    }
+    
+    func makeCoordinator(application: UIApplication) {
+        guard let rootController = application.windows.first?.rootViewController as? CoordinatorNavigationController else {
+            fatalError("rootViewController must be CoordinatorNavigationController")
+        }
+        
+        appCoordinator = AppCoordinator(router: Router(rootController: rootController), container: assembler.resolver)
+        assembler.resolver.resolve(AuthStateObserver.self)!.setCoordinator(appCoordinator)
+        appCoordinator?.start()
+    }
+    
+    private func setupKeyboardManager() {
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.previousNextDisplayMode = .alwaysHide
+        IQKeyboardManager.shared.shouldShowToolbarPlaceholder = false
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
+    }
+    
+    private func setupKingfisher() {
+        let cache = ImageCache.default
+        cache.memoryStorage.config.expiration = .seconds(30)
+        cache.diskStorage.config.expiration = .days(2)
+        cache.memoryStorage.config.totalCostLimit = getMB(10)
+    }
+    
+    private func getMB(_ value: Int) -> Int {
+        return value * 1024 * 1024
+    }
+    
+    private func setupNavigationBar() {
+        let navigationBar = UINavigationBar.appearance()
+        navigationBar.isTranslucent = false
+        navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationBar.shadowImage = UIImage()
+        navigationBar.titleTextAttributes = Constants.titleTextAttributes
+        navigationBar.barTintColor = .white
+    }
+
+    // MARK: - Core Data stack
+
+    lazy var persistentContainer: NSPersistentContainer = {
+        /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+        */
+        let container = NSPersistentContainer(name: "SertLombard")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                 
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
 }
 
+private enum Constants {
+    static let titleTextAttributes: [NSAttributedString.Key: Any] = [
+        :
+    ]
+    static let textViewAttributes: [NSAttributedString.Key: Any] = [
+        :
+    ]
+}
