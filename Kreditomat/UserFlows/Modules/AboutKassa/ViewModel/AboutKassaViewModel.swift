ea5d256235ecbe6d635d347866d8bac5ec82ce04
AsylbekTalgatuly <talgatuly071@gmail.com>
2021-06-14 16:17:15 +0600

    Fix points in aboutkassa

--- Kreditomat/UserFlows/Modules/AboutKassa/ViewModel/AboutKassaViewModel.swift
+++ Kreditomat/UserFlows/Modules/AboutKassa/ViewModel/AboutKassaViewModel.swift
@@ -4,7 +4,7 @@ final class AboutKassaViewModel: ViewModel {
 
     struct Input {
         let typeButton: Observable<Int>
-        let point: Observable<Point>
+        let point: Observable<Int>
         let sum: Observable<String?>
         let succesTapped: Observable<Void>
         let pointList: Observable<Void>
@@ -25,7 +25,7 @@ final class AboutKassaViewModel: ViewModel {
         let result = input.succesTapped
             .withLatestFrom(Observable.combineLatest(input.typeButton, input.sum, input.point))
             .flatMap { [unowned self] type, sum, point in
-                return apiService.makeRequest(to: MainTarget.createCashRequest(type: type, sum: Int(sum ?? "") ?? 0, point: point.SellerID))
+                return apiService.makeRequest(to: MainTarget.createCashRequest(type: type, sum: Int(sum ?? "") ?? 0, point: point))
                     .result(ResponseStatus.self)
                     .asLoadingSequence()
             }.share()
@@ -42,4 +42,3 @@ final class AboutKassaViewModel: ViewModel {
         return .init(nextTapped: result, loadPoint: pointList)
     }
 }
-
