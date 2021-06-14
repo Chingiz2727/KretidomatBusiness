58635dd809d74ed85272b05d1ab916d8e271b9ac
kairzhan8 <33624167+kairzhan8@users.noreply.github.com>
2021-06-14 18:19:02 +0600

    Kaira fixes (#33)
    * fix
    
    * Update KassOperationReportViewController.swift
    
    fix

--- Kreditomat/UserFlows/Modules/CreateCashier/Target/CashierTarget.swift
+++ Kreditomat/UserFlows/Modules/CreateCashier/Target/CashierTarget.swift
@@ -10,6 +10,8 @@ import Foundation
 enum CashierTarget: ApiTarget {
     case getCashiers
     case createCashier(name: String, email: String, phone: String)
+    case attachCashier(sellerId: Int, sellerUserId: Int)
+    case block(sellerId: Int, sellerUserId: Int, type: Int)
     
     var version: ApiVersion {
         .custom("")
@@ -23,6 +25,10 @@ enum CashierTarget: ApiTarget {
             return "GetCashiers"
         case .createCashier:
             return "RegisterCashier"
+        case .attachCashier:
+            return "AttachCashierToPoint"
+        case .block:
+            return "Disable"
         }
     }
     
@@ -30,8 +36,10 @@ enum CashierTarget: ApiTarget {
         switch self {
         case .getCashiers:
             return .get
-        case .createCashier:
+        case .createCashier, .attachCashier:
             return .put
+        case .block:
+            return .post
         }
     }
     
@@ -45,6 +53,10 @@ enum CashierTarget: ApiTarget {
                 "email": email,
                 "phone": phone
             ]
+        case let .attachCashier(sellerId, sellerUserId):
+            return ["sellerId": sellerId, "sellerUserId": sellerUserId]
+        case let .block(sellerId, sellerUserId, type):
+            return ["sellerId": sellerId, "sellerUserId": sellerUserId, "type": type]
         }
     }
     
